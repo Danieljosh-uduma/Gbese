cec3e5124f811c8586391f8a409b40cc3e8a0851
Daniel Josh <danieludumajosh@gmail.com>
2025-04-21 23:51:33 +0100

    setup repo

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div className="App">
+        <h1>Gbese Bank Account</h1>
+      </div>  
+    </>
+
+  )
+}
+
+export default App
