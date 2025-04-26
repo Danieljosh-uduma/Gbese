03f58e9c3e7c1c3d3c71de25b80408ecc761eb1e
Daniel Josh <danieludumajosh@gmail.com>
2025-04-26 08:59:03 +0100

    FE skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
+import MainRouter from './router/index'
 import './App.css'
 
 function App() {
-    const API_KEY = import.meta.env.VITE_API_KEY
+    
   return (
     <>
-      <div className="App">
-        <h1>Gbese Bank Account {API_KEY}</h1>
-      </div>  
+      <h1>Hello world</h1>
+      <MainRouter />
     </>
 
   )
