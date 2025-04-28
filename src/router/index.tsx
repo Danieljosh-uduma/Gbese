433e1226c2fb61b3c969dd5640f6604468bf46c7
Daniel Josh <danieludumajosh@gmail.com>
2025-04-28 16:45:04 +0100

    setup'

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,14 @@
 import { Route, BrowserRouter, Routes } from "react-router";
 import Dashboard from "../pages/Dashboard/Page";
+import Button from "../components/common/button/Button";
 
 
 export default function MainRouter() {
   return (
     <BrowserRouter>
+        <Button size="large" url="signup">
+          go to Google
+        </Button>
         <Routes>
             <Route path="/" element={<Dashboard />} />
         </Routes>
