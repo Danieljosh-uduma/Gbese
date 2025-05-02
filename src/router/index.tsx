828e2128b996d83f9579a8bca32e213037a17329
Daniel Josh <danieludumajosh@gmail.com>
2025-05-02 09:56:40 +0100

    final fix

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,12 @@
-import { Route,  BrowserRouter, Routes } from "react-router-dom"
-import Dashboard from "../pages/Dashboard/Page"
-import SignUpPage from "../pages/signup/SignUpPage"
-import LoginPage from "../pages/Login/LoginPage"
-import MobileIntro1 from "../pages/signup/MobileIntro1"
-import MobileIntro2 from "../pages/signup/MobileIntro2"
-import MobileIntro3 from "../pages/signup/MobileIntro3"
-import NotFound from "../pages/NotFound/NotFound"
+import { Route,  BrowserRouter, Routes } from "react-router-dom";
+import Dashboard from "../pages/Dashboard/Page";
+import SignUpPage from "../pages/signup/SignUpPage";
+import LoginPage from "../pages/Login/LoginPage";
+import MobileIntro1 from "../pages/signup/MobileIntro1"; 
+import MobileIntro2 from "../pages/signup/MobileIntro2";
+import MobileIntro3 from "../pages/signup/MobileIntro3";
+import NotFound from "../pages/NotFound/NotFound";
+import Otp from "../pages/Otp page/Otp";
 
 export default function MainRouter() {
 
@@ -15,6 +16,7 @@ export default function MainRouter() {
             <Route path="/" element={<Dashboard/>} />
             <Route path="/dashboard" element={<Dashboard/>} />
             <Route path="/signup" element={<SignUpPage/>} />
+            <Route path="/verify-otp" element={<Otp/>} />
             <Route path="/login" element={<LoginPage/>} />
             <Route path="/mobileintro1" element={<MobileIntro1/>} />
             <Route path="/mobileintro2" element={<MobileIntro2/>} />
