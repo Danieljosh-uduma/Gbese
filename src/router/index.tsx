2f4b1f5c48a152de5897f3ce65e646dfb000a038
Daniel Josh <danieludumajosh@gmail.com>
2025-05-06 03:00:12 +0100

    done

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -6,7 +6,7 @@ import MobileIntro2 from "../pages/signup/mobile/MobileIntro2";
 import MobileIntro3 from "../pages/signup/mobile/MobileIntro3";
 import NotFound from "../pages/NotFound/NotFound";
 import Otp from "../pages/signup/verifyOtp/Otp";
-import Role from "../pages/signup/role/Role"; // Ensure this file exists at the specified path
+// import Role from "../pages/signup/role/Role"; // Ensure this file exists at the specified path
 import CompleteProfile from "../pages/signup/userDetails/CompleteProfile";
 import SplashManager from "../hooks/SplashManager";
 
@@ -26,7 +26,7 @@ export default function MainRouter() {
             <Route path="/signup" element={<SignUpPage/>} />
             <Route path="/signup/verify-otp" element={<Otp/>} />
             <Route path="/signup/completeprofile" element={<CompleteProfile/>} />
-            <Route path="/signup/completeprofile/role" element={<Role />} />
+            {/* <Route path="/signup/completeprofile/role" element={<Role />} /> */}
 
             {/* mobile intro not done  */}
             <Route path="/mobileintro1" element={<MobileIntro1/>} />
