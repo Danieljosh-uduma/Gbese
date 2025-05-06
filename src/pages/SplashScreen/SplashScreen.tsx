d92fe287881a8149bab86de057b94bd93893037d
Daniel Josh <danieludumajosh@gmail.com>
2025-05-06 02:14:51 +0100

    authentication done

--- src/pages/SplashScreen/SplashScreen.tsx
+++ src/pages/SplashScreen/SplashScreen.tsx
@@ -1,9 +1,10 @@
+import { Logo } from '../../components/icons/Icon'
 import './SplashScreen.css'
 export default function SplashScreen() {
     return (
         <main className="splash-container">
             <div className="splash-content">
-                <img src="/src/assets/images/icons/logo-main.svg" alt="Logo" className='splash-logo' />
+                <Logo className='splash-logo' />
                 <h1 className="splash-title">GBESE</h1>
                 <p className="splash-tagline">Revolutionizing Debt Transfer</p>
                 <div className="splash-loader">
